e6ee8df84339136c5afbe1618421a6fcd143ed3d
andresdavidsv <soy@andresdavidsv.com>
2023-03-01 20:08:20 -0500

    add and config tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Andres Portfolio</title>
       </Head>
-      <h1>Hello</h1>
+      <h1 className="text-red-500">Hello</h1>
     </>
   );
 }
