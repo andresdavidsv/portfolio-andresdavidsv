3e4eec92857478116f9f90b0800abcbd2ee3bd61
andresdavidsv <soy@andresdavidsv.com>
2023-03-06 16:58:22 -0500

    feat: add Experiencies section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { Header } from '@/components/Header';
 import { Hero } from '@/components/Hero';
 import { About } from '@/components/About';
+import { WorkExperience } from '@/components/Experience';
 
 export default function Home() {
   return (
@@ -15,11 +16,14 @@ export default function Home() {
       <section id="hero" className="snap-start">
         <Hero />
       </section>
+      {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* About */}
       {/* Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
       {/* Skills */}
       {/* Project */}
       {/* Contact Me */}
