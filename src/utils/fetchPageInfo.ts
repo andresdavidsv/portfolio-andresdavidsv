4e32f147c53fc419401074765796793894c201aa
andresdavidsv <soy@andresdavidsv.com>
2023-03-06 22:00:05 -0500

    fix: sanity deployment

--- src/utils/fetchPageInfo.ts
+++ src/utils/fetchPageInfo.ts
@@ -1,15 +1,10 @@
 import { PageInfo } from '@/interfaces';
 
 export const fetchPageInfo = async () => {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
-    );
-    const data = await res.json();
-    const pageInfo: PageInfo = data.pageInfo;
-    return pageInfo;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+  );
+  const data = await res.json();
+  const pageInfo: PageInfo = data.pageInfo;
+  return pageInfo;
 };
