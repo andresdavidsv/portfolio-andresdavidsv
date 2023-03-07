89ca1865de1b81a3253e3866c5149ab467e29757
andresdavidsv <soy@andresdavidsv.com>
2023-03-07 00:08:04 -0500

    fix: get server static props

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'pd7k8hqi',
-    dataset: 'production',
+    projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
+    dataset: `${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
   },
 })
