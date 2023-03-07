19935dde6a8809fd9f655606ac83e7e4886b9c70
andresdavidsv <soy@andresdavidsv.com>
2023-03-06 21:15:58 -0500

    fix: sanity deployment

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,3 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'pd7k8hqi',
-    dataset: 'production'
-  }
-})
+export default {
+  api: {},
+}
